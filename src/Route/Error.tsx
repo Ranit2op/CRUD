65601f221baeb8f9d89ded29dc4a0562669bd580
Ranit2op <saharanit196@gmail.com>
2025-01-18 16:23:40 +0530

    Delete src/Route directory

--- src/Route/Error.tsx
+++
@@ -1,9 +0,0 @@
-const Error=()=>{
-    return(
-        <>
-        <h2 style={{color:'red',fontFamily:'sans-serif'}}>Something went wrong</h2>
-        
-        </>
-    )
-}
-export default Error;
